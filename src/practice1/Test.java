acce7b131031cdd179374441eb1b42ee71fb40e2
东方 <eastrisewm@163.com>
2021-02-23 14:55:05 +0800

    ..

--- src/practice1/Test.java
+++ src/practice1/Test.java
@@ -72,7 +72,7 @@ public class Test {
         for (int i = 0; i < testTime; i++) {
             int[] arr1 = generateRandomArray(maxSize, maxValue);
             int[] arr2 = copyArray(arr1);
-            mergeSort(arr1);
+            shellInsertSort(arr1);
             comparator(arr2);
             if (!isEqual(arr1, arr2)) {
                 succeed = false;
@@ -83,57 +83,32 @@ public class Test {
 
         int[] arr = generateRandomArray(maxSize, maxValue);
         printArray(arr);
-        mergeSort(arr);
+        shellInsertSort(arr);
         printArray(arr);
     }
 
 
-
-    private static void mergeSort(int arr[]){
-        if(arr == null || arr.length < 2)
-            return;
-        int l = 0;
-        int r = arr.length - 1;
-        mergeSort(arr,l,r);
-    }
-
-    private static void mergeSort(int arr[],int l,int r){
-        if(l == r)
-            return;
-        int mid = l + ((r-l) >> 1);
-        mergeSort(arr,l,mid);
-        mergeSort(arr,mid+1,r);
-        if(arr[mid]>arr[mid+1])
-            merge(arr,l,mid,r);
-    }
-
-    private static void merge(int[] arr, int l, int mid, int r) {
-        int[] help = new int[r-l+1];
-        int p1 = l;
-        int p2 = mid+1;
-        int i = 0;
-        while (p1<=mid && p2<=r){
-            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
-        }
-
-        while(p1 <= mid)
-            help[i++] = arr[p1++];
-
-        while (p2 <= r)
-            help[i++] = arr[p2++];
-
-        for (i = 0; i < help.length; i++) {
-            arr[l+i] = help[i];
+    private static void shellInsertSort(int arr[]) {
+        int length = arr.length;
+        int i,j;
+        for(int increment = length/2; increment >0; increment/=2){
+            for (i = increment; i < length; i++){
+                int temp = arr[i];
+                for(j = i; j - increment >= 0 && temp < arr[j - increment]; j-= increment){
+                    arr[j] = arr[j - increment];
+                }
+                arr[j] = temp;
+            }
         }
     }
 
-    private static void swap(int[] arr, int i,int j){
-        if(i == j)
+    private static void swap(int[] arr, int i, int j) {
+        if (i == j)
             return;
 //        System.out.println("之前 a = " + arr[i] + "， b = "+arr[j]);
-        arr[i] = arr[i]^arr[j];
-        arr[j] = arr[i]^arr[j];
-        arr[i] = arr[i]^arr[j];
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
 //        System.out.println("之后 a = " + arr[i] + "， b = "+arr[j]);
     }
 }
